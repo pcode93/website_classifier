fba183ce5c320da3820edc1bbb25c6c1093c1759
Arkadiusz Perka <arkadiusz@MacBook-Pro-Arkadiusz.local>
2016-12-09 17:46:22 +0100

    ADD: Sending whole index.html

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
+index_file = open('app/templates/index.html', 'r')
+
 from app.dbs import models
 from app.templates import views
 from app.rest import rest
+
--- app/templates/views.py
+++ app/templates/views.py
@@ -1,11 +1,13 @@
 from flask import render_template, flash, redirect
 from app import app
+from app import index_file
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return index_file.read()
+                #render_template('index.html')
                  #,
                  #                  title='Home',
                  #                  user=user,
