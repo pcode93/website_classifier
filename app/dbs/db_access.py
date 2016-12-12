afae215eae608163ac02459394b6a32af937b6b4
Arkadiusz Perka <arkadiusz@MacBook-Pro-Arkadiusz.local>
2016-12-12 19:47:24 +0100

    ADD: Error handling, jsonify

--- app/dbs/db_access.py
+++ app/dbs/db_access.py
@@ -1,5 +1,4 @@
 from app.dbs import models
-import json
 
 def get_pages( category_name ):
     category = models.Category.query.filter_by(name = category_name ).first()
@@ -11,6 +10,3 @@ def get_urls( category_name ):
         urls.append(page.url)
     return urls
 
-def get_urls_as_json( category_name ):
-    data = get_urls( category_name )
-    return json.dumps({'category': category_name, 'websites': data}, sort_keys=True, indent=4, separators=(',',':'))
--- app/rest/rest.py
+++ app/rest/rest.py
@@ -2,6 +2,8 @@ from app import app
 from app.bayes import classifier
 from app.parse import website_parser
 from app.dbs import db_access
+from urllib2 import URLError, HTTPError
+from flask import jsonify, make_response
 
 @app.route('/classify')
 def api_articles():
@@ -9,4 +11,21 @@ def api_articles():
 
 @app.route('/classify/<path:url>')
 def api_article(url):
-    return db_access.get_urls_as_json(classifier.classify(website_parser.get_text(url)))
+    try:
+        category_name = classifier.classify(website_parser.get_text(url))
+        print
+        print category_name
+        url_list = db_access.get_urls( category_name )
+        return jsonify(category=category_name,
+                       websites=url_list)
+    
+    except HTTPError, e:
+        response = jsonify(error = 'The server couldn\'t fulfill the request.')
+        return make_response(response, e.code)
+    
+    except URLError, e:
+        return make_response(jsonify(error = 'URL ERROR'), 404)
+    
+    except ValueError, e:
+        response = jsonify(error = 'We failed to reach a server. \n\r Did you selected http?')
+        return make_response(response, 404)
