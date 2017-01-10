455fb24d86bbd821b69834214dc62c0beeb75b3e
Paul <P.Chudzik@stud.elka.pw.edu.pl>
2017-01-11 00:05:59 +0100

    scons

--- app/bayes/make.sh
+++
@@ -1,4 +0,0 @@
-g++ -std=c++11 -I /usr/include/python2.7 -fpic -c -o classifier.o classifier.cpp
-g++ -o classifier.so -shared classifier.o -lboost_python -lpython2.7 -lboost_system
-
-g++ -std=c++11 test.cpp -o test -lboost_system -lboost_unit_test_framework
